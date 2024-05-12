30024705f3de9ca9ea6867771b470faa5a0376ea
Mazionach <mazafesio25@gmail.com>
2024-05-12 06:47:01 -0700

    Swapped to fs module

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,2 +1,12 @@
-w = window.open(arguments[0]);
-w.print();
+
+const fs = require('fs');
+
+
+fs.readFile('example.txt', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data.toString());
+});
+
