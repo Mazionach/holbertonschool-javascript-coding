36c299534d0bfaba9c9f72f066589e4f20ffec55
Mazionach <mazafesio25@gmail.com>
2024-07-10 11:47:54 -0700

    Fixed imports

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,17 @@
 
-const { argv } = require('node:process');
+const fs = require('fs');
+
+
+if (!process.argv[2]){
+	console.error('No file path');
+	process.exit(1);
+}
 
 fs.readFile(process.argv[2], (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data.toString());
+  console.log(data);
 });
 
