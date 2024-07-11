a23a2d329444a51d41a693e7acdfc26ec84b3149
Mazionach <mazafesio25@gmail.com>
2024-07-11 17:10:30 +0000

    Added count tasks

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const req = require('request');
+
+req(process.argv[2], (err, resp, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const tasks = {};
+    JSON.parse(body).forEach((t) => {
+      if (t.completed) {
+        if (tasks[t.userId]) {
+          tasks[t.userId] = tasks[t.userId+1];
+        }
+        if (!tasks[t.userId]) {
+          tasks[t.userId] = 1;
+        }
+      }
+    }
+    console.log(tasks);
+  }
+});
